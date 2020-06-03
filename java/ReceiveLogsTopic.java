6a30ca0feca34a92185b8c12d6cb2c2302e3da90
KenkyChao <51088413+KenkyChao@users.noreply.github.com>
2020-06-03 15:36:33 +0800

    Update ReceiveLogsTopic.java

--- java/ReceiveLogsTopic.java
+++ java/ReceiveLogsTopic.java
@@ -8,12 +8,14 @@ public class ReceiveLogsTopic {
     private static final String EXCHANGE_NAME = "topic_logs";
 
     public static void main(String[] argv) throws Exception {
+        //创建服务器连接
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-
+        //指定交换类型
         channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        //在Java客户端中，当我们不向queueDeclare（）提供任何参数时，我们将 使用生成的名称创建一个非持久的，排他的，自动删除的队列：
         String queueName = channel.queueDeclare().getQueue();
 
         if (argv.length < 1) {
@@ -21,12 +23,15 @@ public class ReceiveLogsTopic {
             System.exit(1);
         }
 
+        //交换和队列之间的绑定
         for (String bindingKey : argv) {
             channel.queueBind(queueName, EXCHANGE_NAME, bindingKey);
         }
 
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
+        //我们将告诉服务器将队列中的消息传递给我们。由于它将异步地向我们发送消息，因此我们以对象的形式提供了一个回调，
+        //该回调将缓冲消息，直到我们准备使用它们为止。那就是DeliverCallback子类所做的。
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
